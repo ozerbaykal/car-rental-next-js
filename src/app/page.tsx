aaa14b0f7d09efed520b1e6010224ed223deb076
ozerbaykal <baykalozer87@gmail.com>
2024-11-16 00:08:37 +0300

    assets added,and hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <h1>Ana Sayfa</h1>
+      {/* Hero */}
+      <div className="h-[60vh] grid place-items-center bg-[linear-gradient(#00000084,#00000084),url('./assets/car-bg.jpg')] bg-center bg-cover text-white">
+        <div className="text-center">
+          <p>Yakınındaki kiralık araçları keşfet</p>
+          <h1 className="text-5xl font-bold mt-5">Senin İçin Mükemmel Aracı Bul</h1>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
