f1e210afc192c92962ad11c0e7be656b72dc5bb5
ozerbaykal <baykalozer87@gmail.com>
2024-11-15 23:14:45 +0300

    project created first commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link"
+
+const Header = () => {
+    return (
+        <header>
+            <h1>DRIVE</h1>
+            <nav>
+                <Link href="/"> Browse
+                </Link>
+            </nav>
+
+
+
+
+
+        </header>
+    )
+}
+
+export default Header
