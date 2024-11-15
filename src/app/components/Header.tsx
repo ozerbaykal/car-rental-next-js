4d2539489614662b7f1c48c10ca3071a6552ee8b
ozerbaykal <baykalozer87@gmail.com>
2024-11-15 23:51:31 +0300

    header and container_hoc

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,20 +1,29 @@
-import Link from "next/link"
-
+import Link from "next/link";
+import Container from "./Container";
+Container
 const Header = () => {
     return (
-        <header>
-            <h1>DRIVE</h1>
-            <nav>
-                <Link href="/"> Browse
-                </Link>
-            </nav>
+        <header >
+            <Container designs="flex justify-between items-center w-full ">
+                <h1 className="font-bold text-2xl md:text-3xl">
+                    <Link href="/">DRIVE</Link>
+                </h1>
 
+                <nav className="text-sm md:text-base flex gap-2 md:gap-4">
+                    <Link href="/"> Browse</Link>
+                    <Link href="/"> Siparişlerim</Link>
+                    <Link href="/"> Hakkımızda</Link>
+                    <Link className="max-md:hidden" href="/"> Fİyatlandırma</Link>
+                </nav>
 
-
-
+                <div className="flex gap-2">
+                    <button className="max-md:hidden" >Contact</button>
+                    <button className="bg-white text-black px-5 py-2 rounded-full text-nowrap transiton hover:bg-gray-100">Sign Up</button>
+                </div>
+            </Container>
 
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
